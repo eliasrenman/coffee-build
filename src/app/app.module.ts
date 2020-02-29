76f0656cf0430dc90cc78feb3001518c1565ae0f
Elias Renman <elias.renman00@gmail.com>
2020-02-29 19:34:44 +0100

    created starting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LogoComponent } from './assets/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
