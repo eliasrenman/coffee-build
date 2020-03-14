18a3e19d985429aea94c032bcd42baa0b875782c
abborren <elias.renman00@gmail.com>
2020-03-14 12:49:21 +0100

    end of road

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
-module.exports = {
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+      './src/**/*.html',
+    ],
+  
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  });
+  
+  module.exports = {
     module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                loader: 'postcss-loader',
-                options: {
-                    ident: 'postcss',
-                    syntax: 'postcss-scss',
-                    plugins: () => [
-                        require('postcss-import'),
-                        require('tailwindcss'),
-                        require('autoprefixer'),
-                    ]
-                }
-            }
-        ]
+      rules: [
+        {
+          test: /\.scss$/,
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            syntax: 'postcss-scss',
+            plugins: () => [
+              require('postcss-import'),
+              require('tailwindcss'),
+              purgecss,
+              require('autoprefixer'),
+            ]
+          }
+        }
+      ]
     }
-};
+  };
