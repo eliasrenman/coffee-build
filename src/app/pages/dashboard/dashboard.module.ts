7833414943518ceebf42f23ad26547e995d09200
Elias Renman <elias.renman00@gmail.com>
2020-08-02 18:06:15 +0200

    finished up basic layout of subscribed devices

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
+import { SubscribedDevicesComponent } from './subscribed-devices/subscribed-devices.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { UserMenuComponent } from './toolbar/user-menu/user-menu.component';
 import { UniqueEndpointComponent } from './unique-endpoint/unique-endpoint.component';
-import { SubscribedDevicesComponent } from './subscribed-devices/subscribed-devices.component';
 import { UsageGuideComponent } from './usage-guide/usage-guide.component';
 
 
@@ -20,13 +22,22 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [DashboardComponent, ToolbarComponent, UserMenuComponent, UniqueEndpointComponent, SubscribedDevicesComponent, UsageGuideComponent],
+  declarations: [
+    DashboardComponent,
+    ToolbarComponent,
+    UserMenuComponent,
+    UniqueEndpointComponent,
+    SubscribedDevicesComponent,
+    UsageGuideComponent
+  ],
   imports: [
     CommonModule,
+    MatButtonModule,
     MatIconModule,
     MatMenuModule,
     MatDividerModule,
     MatDialogModule,
+    MatListModule,
     SharedModule,
     RouterModule.forChild(routes)
   ],
