88ba95daee081f1d4463fd79f9348c6f20746754
Elias Renman <elias.renman00@gmail.com>
2020-08-11 15:57:10 +0200

    finished usage guide

--- src/app/pages/dashboard/usage-guide/usage-guide.component.ts
+++ src/app/pages/dashboard/usage-guide/usage-guide.component.ts
@@ -1,7 +1,7 @@
 import { Subscription } from 'rxjs';
 import { Endpoint } from './../../../shared/api-endpoints';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from 'src/app/shared/services/auth/user.model';
 
 @Component({
@@ -9,12 +9,17 @@ import { User } from 'src/app/shared/services/auth/user.model';
   templateUrl: './usage-guide.component.html',
   styleUrls: ['./usage-guide.component.scss']
 })
-export class UsageGuideComponent implements OnInit {
+export class UsageGuideComponent implements OnInit, OnDestroy {
 
   user: User;
   userSub: Subscription;
 
   constructor(private auth: AuthService) { }
+  ngOnDestroy(): void {
+    if(this.user) {
+      this.userSub.unsubscribe();
+    }
+  }
 
   ngOnInit(): void {
     this.userSub = this.auth.user.subscribe(user => {
